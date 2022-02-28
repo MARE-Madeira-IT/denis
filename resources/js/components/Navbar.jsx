6271c062d0e0df5a77e53a78ebc447fa325b4dd0
rubenf98 <joseruben98@hotmail.com>
2022-02-28 10:49:10 +0000

    fixed overlapping navbar

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,63 +1,63 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Container = styled.div`
+const FlexContainer = styled.div`
     position: fixed;
-    left: 0%;
     top: 0%;
-    right: 0%;
     bottom: auto;
     z-index: 990;
     width: 100%;
     text-transform: uppercase;
     color: white;
+`;
+
+const Logo = styled(FlexContainer)`
+    left:48px;
+    top: 48px;
+
+    h1 {
+        font-size: 48px;
+        margin-bottom: 10px;
+        margin-top: 0px;
+    }
+
+    p {
+        margin-top: 0px;
+        font-size: 18px;
+    }
     
 `;
 
-const Flex = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 48px;
 
-    .column {
+const Login = styled(FlexContainer)`
+    right:48px;
+    top: 48px;
+    width: 35px;
 
-        h1 {
-            font-size: 48px;
-            margin-bottom: 10px;
-        }
-
-        p {
-            margin-top: 0px;
-            font-size: 18px;
-        }
-
+    .link {
         img {
-            width: 35px;
+            width: 100%;
             float: right;
             cursor: pointer;
-        }
-    }
-
-    
+        }     
+    }   
 `;
 
 function Navbar() {
     return (
-        <Container>
-            <Flex>
-                <div className='column' style={{ width: "50%" }}>
-                    <h1>LOREM</h1>
-                    <p>Lorem ipsum dolor.</p>
-
-                </div>
-                <Link className='column' to="/dashboard">
+        <Fragment>
+            <Logo>
+                <h1>LOREM</h1>
+                <p>Lorem ipsum dolor.</p>
+            </Logo>
+            <Login>
+                <Link className='link' to="/dashboard">
                     <img src="/images/navbar/login.svg" alt="" />
 
                 </Link>
-            </Flex>
-        </Container>
+            </Login>
+        </Fragment>
     )
 }
 
